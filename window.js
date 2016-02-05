0413c18c81d0048217318b72692e819e2f3a201e
cesarve77 <c@imagenproactiva.com>
2016-02-05 15:21:02 -0430

    remove console log

--- window.js
+++ window.js
@@ -18,13 +18,11 @@ SimpleChat.scrollToEnd = function (template) {
 }
 
 Template.SimpleChatWindow.onCreated(function () {
-    console.log('created')
     this.data.initializing = true;
     this.data.subscribing = true;
     this.data.title = this.data.title || SimpleChat.options.title
     this.data.minimize = this.data.minimize || SimpleChat.options.minimize
     this.data.limit = new ReactiveVar(this.data.limit || SimpleChat.options.limit)
-    console.log('on created', this.data.limit.get())
     this.data.beep = this.data.beep || SimpleChat.options.beep
     this.data.increment = this.data.limit.get()
     if (typeof this.data.roomId != "function")
@@ -50,11 +48,7 @@ Template.SimpleChatWindow.onCreated(function () {
 });
 
 Template.SimpleChatWindow.onRendered(function () {
-    this.autorun(() => {
-        console.log('=======>========>=========>subs123', this.getRoomId())
 
-    })
-    console.log('rendered')
     var self = this
     self.endScroll = true;
     this.$('.direct-chat-messages').scroll(function (event) {
@@ -71,7 +65,6 @@ Template.SimpleChatWindow.onRendered(function () {
             /**
              * the setTimeOut is to be sure that dom already update, and make the real calc of scroñ
              */
-            console.log('SCROLLLLLL', this.data.scroll, this.$('.direct-chat-messages')[0].scrollHeight)
             this.$('.direct-chat-messages').scrollTop(this.$('.scroll-height')[0].scrollHeight - this.data.scroll)
 
 
@@ -89,18 +82,14 @@ Template.SimpleChatWindow.onRendered(function () {
     })
 
     $(window).on('SimpleChat.newMessage', (e, doc)=> {
-        console.log('**************nwe message', doc, this.endScroll)
         if (this.endScroll) {
 
-            console.log('scrollToEnd', this)
             SimpleChat.scrollToEnd(this)
-            if (window.visivility == 'hidden' && this.data.beep) {
-                console.log("beep111")
+            if (this.data.beep && window.visivility == 'hidden' ) {
                 new Audio('/packages/cesarve_simple-chat/assets/bell.mp3').play()
             }
         } else {
-            if (this.getUsername() != doc.username && this.data.beep) {
-                console.log("beep22")
+            if (this.data.beep  && this.getUsername() != doc.username) {
                 new Audio('/packages/cesarve_simple-chat/assets/bell.mp3').play()
             }
         }
@@ -112,14 +101,11 @@ Template.SimpleChatWindow.helpers({
 
     simpleChats: function () {
 
-        console.log(1, this)
-        console.log('this.roomId1', Template.instance().getRoomId())
         var chats = SimpleChat.Chats.find({roomId: Template.instance().getRoomId()}, {sort: {date: 1}})
 
         let handleChanges = chats.observeChanges({
             added: (id, doc) => {
                 if (!this.subscribing) {
-                    console.log('this.subscribing', this.subscribing)
                     $(window).trigger('SimpleChat.newMessage', [doc])
                 }
             }
@@ -128,7 +114,6 @@ Template.SimpleChatWindow.helpers({
         return chats;
     },
     hasMore: function () {
-        console.log('this.limit.get()',  this.limit)
         return SimpleChat.Chats.find({roomId: Template.instance().getRoomId()}, {
                 sort: {date: 1},
                 limit: this.limit.get()
@@ -136,7 +121,6 @@ Template.SimpleChatWindow.helpers({
     },
 
     me: function () {
-        console.log('me', this)
         return Template.instance().getUsername() == this.username
     }
     ,
@@ -154,17 +138,16 @@ Template.SimpleChatWindow.helpers({
 
 Template.SimpleChatWindow.events({
     'click #simple-chat-load-more': function () {
-        console.log('event this', this)
         this.limit.set(this.limit.get() + this.increment)
         this.scroll = Template.instance().$('.scroll-height')[0].scrollHeight
-
+        Template.instance().$(".direct-chat-messages").animate({scrollTop: 0}, 0);
+        Template.instance().$('.direct-chat-messages').trigger('scroll')
 
     },
     'keydown #simple-chat-message': function (event) {
         var $message = $(event.currentTarget)
         if (event.which == 13 && $message.val() != '') { // 13 is the enter key event
             event.preventDefault()
-            console.log('Template.instance().$(button)', Template.instance().$('button'))
             Template.instance().$('button').click()
         }
     },
