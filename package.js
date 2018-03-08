aaa04a78d4610d322d6d3d818c58a65f2c1db6e5
Massimo Menichinelli <info@openp2pdesign.org>
2018-03-08 22:59:46 +0100

    Bump to 0.4.3, this fixes issues #10 #13 #18 #20 #22

--- package.js
+++ package.js
@@ -1,6 +1,6 @@
 Package.describe({
     summary: "Simple chat window. The star point to make your own chat",
-    version: "0.4.2",
+    version: "0.4.3",
     name: "cesarve:simple-chat",
     git: "https://github.com/cesarve77/simple-chat"
 });
